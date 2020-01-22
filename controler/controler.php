e4ebe29bab386f65a7f1885290030aa3150aa790
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2020-01-22 02:10:36 +0100

    update

--- controler/controler.php
+++ controler/controler.php
@@ -19,31 +19,30 @@ function cklogin($username, $password)
 {
 
     $users = getUsers();
-var_dump((isset($_POST['uname']) && isset($_POST['password'])));
-var_dump($_POST['uname']);
-var_dump($password);
+    var_dump((isset($_POST['uname']) && isset($_POST['password'])));
+    var_dump($_POST['uname']);
+    var_dump($password);
     var_dump($_SESSION);
     if (isset($username) && isset($password)) {
         foreach ($users as $user) {
             var_dump($users);
-            if ( $user["user"] == $username  && $user["password"] == $password) {
+            if ($user["user"] == $username && $user["password"] == $password) {
 
                 $_SESSION['username'] = $username;
 
-                $message =  '<p class=\"alert alert-success\" role=\"alert\"> Welcom '. $_SESSION["username"] . "</p>";
+                $message = '<p class=\"alert alert-success\" role=\"alert\"> Welcom ' . $_SESSION["username"] . "</p>";
             }
         }
     }
-var_dump($_SESSION);
+    var_dump($_SESSION);
     var_dump(isset($_SESSION['username']));
-    if (isset($_SESSION['username']) == false){
-        $message =  '<p class=\"alert alert-success\" role=\"alert\"> This User dose not exist '. $_SESSION["username"] . "</p>";
+    if (isset($_SESSION['username']) == false) {
+        $message = '<p class=\"alert alert-success\" role=\"alert\"> This User dose not exist ' . $_SESSION["username"] . "</p>";
         require_once 'view/login.php';
-    }else{
+    } else {
         require_once 'view/loginsuccess.php';
-
-
-
+    }
+}
 function logout(){
     unset($_SESSION['username']);
     $news = getNews();
